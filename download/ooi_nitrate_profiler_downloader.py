7b8241f855cadbbf38649281f523f9792017f169
andrew-s28 <ascherer97@gmail.com>
2023-10-25 11:22:02 -0700

    add command line argument parser added quality checking removed unneeded variables

--- download/ooi_nitrate_profiler_downloader.py
+++ download/ooi_nitrate_profiler_downloader.py
@@ -1,18 +1,33 @@
-# input/output, html parsing, requests, and os functionality
+"""
+Download OOI Endurance Array profiler nitrate data, QC fits, bin, and save.
+"""
+import argparse
+import gsw
 import io
+import numpy as np
 import os
 import requests
 from tqdm import tqdm as tq
-import numpy as np
-import xarray as xr
-import gsw
 import util
+import xarray as xr
 
-# get nitrate data
-print("Oregon Inshore - CE01ISSP")
-print("Oregon Shelf   - CE02SHSP")
-print("Enter site for nitrate: ", end='')
-site = input()
+# parse command line arguments
+parser = argparse.ArgumentParser(description="Download OOI Endurance Array profiler nitrate data,"
+                                 + " QC fits, bin, and save.")
+parser.add_argument('site', metavar='site', type=str, nargs=1,
+                    help="site to download from")
+parser.add_argument('-p', '--path', metavar='path', type=str, nargs='?',
+                    help="path to folder for output files",
+                    default='./output/')
+parser.add_argument('-f', '--file', metavar='file', type=str, nargs='?',
+                    help="file name for output file, do not include .nc",
+                    default=None)
+args = parser.parse_args()
+args = vars(args)
+site = args['site'][0].upper()
+out_path = args['path']
+out_file = args['file']
+
 # setup defaults to use in subsequent data queries
 refdes = site + "-SP001-06-NUTNRJ000"
 method = "recovered_cspp"
@@ -21,17 +36,15 @@ stream = "nutnr_j_cspp_instrument_recovered"
 # construct the OOI Gold Copy THREDDS catalog URL for this data set
 base_url = "https://thredds.dataexplorer.oceanobservatories.org/thredds/catalog/ooigoldcopy/public/"
 url = base_url + ('-').join([refdes, method, stream]) + '/catalog.html'
-
-tag = r'NUTNRJ000.*.nc$'  # setup regex for files we want (*=anything,$=end of line)
+tag = r'NUTNRJ000.*.nc$'  # setup regex for files we want
 nc_files = util.list_files(url, tag)
-file_url = 'https://thredds.dataexplorer.oceanobservatories.org/thredds/fileServer/'
-nc_url = [file_url + i + '#mode=bytes' for i in nc_files]  # combine files, add mode to ensure download works
+base_url = 'https://thredds.dataexplorer.oceanobservatories.org/thredds/fileServer/'
+nc_url = [base_url + i + '#mode=bytes' for i in nc_files]  # create urls for download
 
 # load datasets
-ds = []  # empty arrays for datasets
+ds = []
 for i, f in (enumerate(tq(nc_url, desc='Downloading datasets'))):
     r = requests.get(f, timeout=(3.05, 120))
-    # ensure request worked
     if r.ok:
         ds.append(xr.load_dataset(io.BytesIO(r.content)))
         ds[i].load()
@@ -40,7 +53,8 @@ for i, f in (enumerate(tq(nc_url, desc='Downloading datasets'))):
 for i, d in enumerate(ds):
     ds[i] = ds[i].swap_dims({'obs': 'time'})
     ds[i] = ds[i].rename({'ctdpf_j_cspp_instrument_recovered-sea_water_temperature': 'temperature',
-                          'ctdpf_j_cspp_instrument_recovered-sea_water_practical_salinity': 'salinity'})
+                          'ctdpf_j_cspp_instrument_recovered-sea_water_practical_salinity': 'salinity',
+                          'nutnr_dark_value_used_for_fit': 'dark_val'})
     ds[i]['abs_sal'] = gsw.conversions.SA_from_SP(ds[i]['salinity'], ds[i]['int_ctd_pressure'],
                                                   ds[i]['lon'], ds[i]['lat'])
     ds[i]['abs_sal'].attrs = {'comment': 'Absolute salinity calculated from pressure and practical salinity.',
@@ -53,49 +67,52 @@ for i, d in enumerate(ds):
     ds[i]['density_anom'].attrs = {'comment': 'Density anomaly - density - 1000 kg/m^3',
                                    'units': 'kg/m^3'}
 
+# QC nitrate data and remove short datasets
+mask = []
+for i, d in enumerate(tq(ds, desc='QC')):
+    ds[i] = util.nutnr_qc(ds[i])
+    if len(ds[i].time) > 10:
+        mask.append(i)
+ds = [ds[i] for i in mask]
+
 # find minimum and maximum depth bins over all nitrate data
 sur = np.min(xr.concat(ds, dim='time')['depth'])
 bot = np.max(xr.concat(ds, dim='time')['depth'])
 
+# setup depth/pressure bins
 step = 1
 sur = np.floor(sur)
 bot = np.ceil(bot)
-# pressure_grid = centers of bins
+# pressure_grid is centers of bins
 pressure_grid = np.arange(sur+step/2, bot+step, step)
-# pressure_bins = edges of bins
+# pressure_bins is edges of bins
 pressure_bins = np.nan*np.empty(len(pressure_grid)+1)
 pressure_bins[0] = pressure_grid[0] - step/2
 pressure_bins[-1] = pressure_grid[-1] + step/2
 for i in range(len(pressure_bins)-2):
     pressure_bins[i+1] = np.average([pressure_grid[i], pressure_grid[i+1]])
 
-# create regular time bins based on nitrate time range
-# need to cast to daily data to round down then recast to ns for consistency
-begin = ds[0]['time'].values[0].astype('datetime64[D]').astype('datetime64[ns]')
-# same as above, but add one extra day since arange is endpoint exclusive
-end = (ds[-1]['time'].values[-1] + np.timedelta64(1, 'D')).astype('datetime64[D]').astype('datetime64[ns]')
-time_grid = np.arange(begin, end, step=np.timedelta64(1, 'D'), dtype='datetime64')
-
-nitrate_grid = np.nan*np.empty([len(pressure_grid), len(time_grid)])
-density_grid = np.nan*np.empty([len(pressure_grid), len(time_grid)])
-sal_grid = np.nan*np.empty([len(pressure_grid), len(time_grid)])
-temp_grid = np.nan*np.empty([len(pressure_grid), len(time_grid)])
-
+# bin datasets into new list
 datasets = []
-
 for d in tq(ds, desc='Binning datasets'):
     datasets.append(util.profiler_binning(d, pressure_bins, offset=0.5))
 
+# concatenate binned datasets
 ds_bin = xr.concat(datasets, dim='time')
 ds_bin = ds_bin.drop_duplicates('time', 'first')
+ds_bin = ds_bin.where(~np.isinf(ds_bin.salinity_corrected_nitrate))
 
-print("Enter name for output nc file: ", end='')
-out_name = input()
+# setup output folders
+if not os.path.exists(out_path):
+    os.mkdir(out_path)
+if not os.path.exists(os.path.join(out_path, 'raw')):
+    os.mkdir(os.path.join(out_path, 'raw'))
+if out_file is None:
+    out_file = site + '_nitrate_binned.nc'
+else:
+    out_file = out_file + '.nc'
 
-if not os.path.exists('./output/'):
-    os.mkdir('./output/')
-
-ds_bin.to_netcdf('./output/' + out_name + '.nc')
-
+# save output files
+ds_bin.to_netcdf(os.path.join(out_path, out_file))
 for d in ds:
-    d.to_netcdf(f'./output/dep{d.deployment[0]:02d}_' + d.attrs['source'] + '.nc')
+    d.to_netcdf(os.path.join(out_path, f'raw/dep{d.deployment[0]:02.0f}_' + d.attrs['source'] + '.nc'))
